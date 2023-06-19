07fdc0c3b02df4743f27ef255f171f3af69be0bd
Joshua Brinson <joshbrin2000@gmail.com>
2023-06-19 16:57:52 -0230

    javascript gameBoard module created

---
+++ script.js
@@ -0,0 +1,25 @@
+const gameBoard = (() => {
+    const boardArray = [];
+    return {
+        init: function () {
+            boardArray = [];
+            for (let i = 0; i < 9; i++){
+                boardArray.push(" ");
+            }
+        },
+
+        setToken: function (tok, ind) {
+            boardArray[ind] = tok;
+        },
+
+        getTokens: function (tok) {
+            let indIndex = [];
+            for (let i = 0; i < 9; i++){
+                if (boardArray[i] === tok){
+                    indIndex.push(i);
+                }
+            }
+            return indIndex;
+        }
+    }
+})();
